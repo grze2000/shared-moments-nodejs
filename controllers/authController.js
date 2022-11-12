36bbec791a46f0116508db62f3890e2437ea1a45
grze2000 <programista333@gmail.com>
2022-11-12 01:29:41 +0100

    auto login after register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,44 +1,52 @@
 import { validationResult } from "express-validator";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { User } from "../models/User.js";
 import { RefreshToken } from "../models/RefreshToken.js";
 
 export const login = (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
 
-  User.findOne({email: req.body.email}, (err, user) => {
-    if(err) {
-      return res.status(500).json({message: 'Database error'});
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return res.status(500).json({ message: "Database error" });
     }
-    if(!user) {
-      return res.status(400).json({message: 'Nieprawidłowy email lub hasło'});
+    if (!user) {
+      return res.status(400).json({ message: "Nieprawidłowy email lub hasło" });
     }
     user.comparePassword(req.body.password, (err, isMatch) => {
-      if(!err && isMatch) {
-        const expirationDate = new Date(Date.now() + 1000*60*60*24*30);
-        const accessToken = jwt.sign({
-          id: user._id,
-        }, process.env.SECRET, {
-          expiresIn: 60*15,
-        });
+      if (!err && isMatch) {
+        const expirationDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+        const accessToken = jwt.sign(
+          {
+            id: user._id,
+          },
+          process.env.SECRET,
+          {
+            expiresIn: 60 * 15,
+          }
+        );
 
-        const refreshToken = jwt.sign({
-          id: user._id,
-        }, process.env.REFRESH_TOKEN_SECRET, {
-          expiresIn: 60*60*24*30,
-        });
+        const refreshToken = jwt.sign(
+          {
+            id: user._id,
+          },
+          process.env.REFRESH_TOKEN_SECRET,
+          {
+            expiresIn: 60 * 60 * 24 * 30,
+          }
+        );
 
         const newRefreshToken = new RefreshToken({
           token: refreshToken,
           expireAt: expirationDate,
         });
 
-        newRefreshToken.save(err => {
-          if(err) {
-            res.status(500).json({message: 'Database error'});
+        newRefreshToken.save((err) => {
+          if (err) {
+            res.status(500).json({ message: "Database error" });
           } else {
             res.json({
               accessToken,
@@ -49,88 +57,142 @@ export const login = (req, res) => {
           }
         });
       } else {
-        res.status(400).json({message: 'Nieprawidłowy email lub hasło'});
+        res.status(400).json({ message: "Nieprawidłowy email lub hasło" });
       }
     });
   });
-}
+};
 
 export const register = (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
 
-  const {fullname, email, password} = req.body;
+  const { fullname, email, password } = req.body;
 
-  User.findOne({
-    email
-  }, (err, user) => {
-    if(err) {
-      return res.status(500).json({message: 'Database error'});
-    }
-    if(user) {
-      return res.status(400).json({message: 'Użytkownik z takim mailem już istnieje'});
-    }
-    User({
-      fullname,
+  User.findOne(
+    {
       email,
-      password,
-    }).save(err => {
-      if(err) {
-        return res.status(500).json({message: 'Database error'});
+    },
+    (err, user) => {
+      if (err) {
+        return res.status(500).json({ message: "Database error" });
       }
-      return res.status(201).json({message: 'Konto zostało utworzone!'});
-    })
-  });
-}
-
-export const refreshToken = (req, res) => {
-  RefreshToken.findOne({token: req.body.token}, (err, token) => {
-    if(err) {
-      return res.status(500).json({message: 'Database error'});
-    }
-    if(!token) {
-      return res.status(400).json({message: 'Nieprawidłowy token'});
-    }
-    jwt.verify(req.body.token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-      if(err || !user) {
-        return res.status(400).json({message: 'Nieprawidłowy token'});
+      if (user) {
+        return res
+          .status(400)
+          .json({ message: "Użytkownik z takim mailem już istnieje" });
       }
-
-      const expirationDate = new Date(Date.now() + 1000*60*60*24*30);
-      const accessToken = jwt.sign({
-        id: user.id,
-      }, process.env.SECRET, {
-        expiresIn: 60*15,
-      });
-      const refreshToken = jwt.sign({
-        id: user.id,
-      }, process.env.REFRESH_TOKEN_SECRET, {
-        expiresIn: 60*60*24*30,
-      });
-
-      token.token = refreshToken;
-      token.expireAt = expirationDate;
-      token.save(err => {
-        if(err) {
-          return res.status(500).json({message: 'Database error'});
+      User({
+        fullname,
+        email,
+        password,
+      }).save((err, user) => {
+        if (err) {
+          return res.status(500).json({ message: "Database error" });
         }
-        res.json({
-          accessToken,
-          refreshToken,
+        const expirationDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+        const accessToken = jwt.sign(
+          {
+            id: user._id,
+          },
+          process.env.SECRET,
+          {
+            expiresIn: 60 * 15,
+          }
+        );
+
+        const refreshToken = jwt.sign(
+          {
+            id: user._id,
+          },
+          process.env.REFRESH_TOKEN_SECRET,
+          {
+            expiresIn: 60 * 60 * 24 * 30,
+          }
+        );
+
+        const newRefreshToken = new RefreshToken({
+          token: refreshToken,
+          expireAt: expirationDate,
+        });
+
+        newRefreshToken.save((err) => {
+          if (err) {
+            return res.status(500).json({ message: "Database error" });
+          } else {
+            return res.status(201).json({
+              accessToken,
+              refreshToken,
+              userId: user._id,
+              email: user.email,
+            });
+          }
         });
       });
-    });
+    }
+  );
+};
+
+export const refreshToken = (req, res) => {
+  RefreshToken.findOne({ token: req.body.token }, (err, token) => {
+    if (err) {
+      return res.status(500).json({ message: "Database error" });
+    }
+    if (!token) {
+      return res.status(400).json({ message: "Nieprawidłowy token" });
+    }
+    jwt.verify(
+      req.body.token,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, user) => {
+        if (err || !user) {
+          return res.status(400).json({ message: "Nieprawidłowy token" });
+        }
+
+        const expirationDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+        const accessToken = jwt.sign(
+          {
+            id: user.id,
+          },
+          process.env.SECRET,
+          {
+            expiresIn: 60 * 15,
+          }
+        );
+        const refreshToken = jwt.sign(
+          {
+            id: user.id,
+          },
+          process.env.REFRESH_TOKEN_SECRET,
+          {
+            expiresIn: 60 * 60 * 24 * 30,
+          }
+        );
+
+        token.token = refreshToken;
+        token.expireAt = expirationDate;
+        token.save((err) => {
+          if (err) {
+            return res.status(500).json({ message: "Database error" });
+          }
+          res.json({
+            accessToken,
+            refreshToken,
+          });
+        });
+      }
+    );
   });
-}
+};
 
 export const revokeToken = (req, res) => {
-  RefreshToken.findOneAndDelete({token: req.body.token}, (err, token) => {
-    if(err || !token) {
-      res.status(400).json({message: 'Nieprawidłowy token'});
+  RefreshToken.findOneAndDelete({ token: req.body.token }, (err, token) => {
+    if (err || !token) {
+      res.status(400).json({ message: "Nieprawidłowy token" });
     } else {
       res.sendStatus(200);
     }
   });
-}
+};
